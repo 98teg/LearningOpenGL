42624e9bb4ca1f2f0a628b8b17096589079db2a6
98teg <teg98tom@gmail.com>
2019-01-19 13:37:51 +0100

    Instaling glew and glut and testing them

---
+++ Ep1/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+#include <GLFW/glfw3.h>
+
+int main(void){
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err){
+	  /* Problem: glewInit failed, something is seriously wrong. */
+	  std::cerr << "Error: " << glewGetErrorString(err);
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(0.0f, -1.0f);
+		glVertex2f(-1.0f, 0.5f);
+		glVertex2f(1.0f, 0.5f);
+		glEnd();
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
